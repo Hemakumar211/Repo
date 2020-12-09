7532f085c713dc842906d04862e49623708e3957
Hemakumar211 <khema74@gmail.com>
2020-12-09 12:34:12 +0530

    reverting the changes

--- hema.java
+++ hema.java
@@ -7,8 +7,3 @@ love u baangaram
 u r such a kind hearted girl
 really feeling so good for having u in my life
 
-<<<<<<< HEAD
-this is from master branch
-=======
-i wanna fuck you so badly darling trunk branch
->>>>>>> trunk
